b1079b6341634c133c245ccd58a1c97f9cfced10
Sourav Acharya Chowdhury <sac642@gmail.com>
2018-09-04 22:18:27 +0530

    Add files via upload

---
+++ webscrapper/webscrapper/spiders/Yobit_Bot.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import csv
+from os import path
+import datetime
+from selenium import webdriver as wd
+from selenium.webdriver.chrome.options import Options
+import time
+
+def csv_output(spyder_name, dic):
+        file_name = "%s.csv"%(spyder_name)
+        if not path.exists(file_name):
+            with open(file_name,'a+',newline='') as csv_file:
+                writer=csv.writer(csv_file)
+                writer.writerow(dic.keys())
+        with open(file_name,'a+',newline='') as csv_file:
+            writer=csv.writer(csv_file)
+            writer.writerow(dic.values())
+
+class YobitBotSpider(scrapy.Spider):
+    name = 'Yobit_Bot'
+    allowed_domains = ['yobit.net/en']
+    start_urls = ['https://yobit.net/en/trade/ETH/BTC/']
+    
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+    }
+    
+    def __init__(self):
+        options = Options()
+        options.set_headless(headless=True)
+        #options.add_argument('--disable-gpu')
+        #options.add_argument('window-size=1366x168')
+        #options.addArgument("--start-maximized");
+        self.driver =wd.Chrome("E:/chromedriver.exe",chrome_options=options)
+        self.driver.maximize_window()
+
+    def parse(self, response):
+        self.driver.implicitly_wait(15)
+        self.driver.get(response.url);
+        time.sleep(10)
+        result=dict()
+        result['Datetime']=datetime.datetime.now()
+        result['Last Current price']=self.driver.find_element_by_xpath('//*[@id="label_last"]').text
+        
+        result['24 Hr Price Change']="NA"
+        
+        result['24 Hr High Price']=self.driver.find_element_by_xpath('//*[@id="label_high24"]').text
+        
+        result['24 Hr Low Price']= self.driver.find_element_by_xpath('//*[@id="label_vol24"]').text
+        
+        result['24 Hr Volume']= self.driver.find_element_by_xpath('//*[@id="label_vol24"]').text.split()[0]
+        
+        result['Coins']= "Bitcoin"
+        
+        result['Base Currency']=self.driver.find_element_by_xpath('//*[@id="data-pjax-container"]/div[1]/ul/li[1]').text.split('/')[1]
+        
+        result['Exchanges URL']=response.url
+        
+        result['Exchanges']="Yobit"
+        
+        csv_output(self.name, result)
+        
